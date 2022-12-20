30a39516c2447f3865c7993ea71e44d9718bdb06
3str-webdev <3str.webdev@gmail.com>
2022-12-20 19:00:07 +0300

    fix: scroll to top on navigation

--- src/components/Layout/Content.tsx
+++ src/components/Layout/Content.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import { FC, useRef, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 interface ContentProps {
   children: React.ReactNode;
@@ -6,9 +7,20 @@ interface ContentProps {
 }
 
 const Content: FC<ContentProps> = ({ children, footer = <></> }) => {
+  const topRef = useRef<HTMLDivElement>(null);
+  const location = useLocation().pathname;
+
+  const scrollToTop = () => {
+    if (topRef.current) topRef.current.scroll({ top: 0 });
+  };
+
+  useEffect(() => {
+    scrollToTop();
+  }, [location]);
+
   return (
     <section className="react-content">
-      <div className="wrapper">
+      <div className="wrapper" ref={topRef}>
         <>
           <div className="react-content-wrapper">{children}</div>
           {footer}
